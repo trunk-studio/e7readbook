8da118d5ac8a4812308fd09cb8944b7ec0653eb3
fuyaode <dan82625@gmail.com>
2015-11-30 17:14:05 +0800

    /app add feeback

---
+++ app/js/feedback.js
@@ -0,0 +1,22 @@
+$$(document).on('pageInit', '.page[data-page="feedback"]', function (e) {
+  $$('#feedback-form').on('submitted', function (e) {
+    var xhr = e.detail.xhr; // actual XHR object
+    var data = e.detail.data;
+    console.log(data);
+    if(data == 'PRINYAL'){
+      mainView.router.back();
+      myApp.addNotification({
+        title: '回報成功',
+        hold: 2000
+      });
+    }else{
+      myApp.alert('請輸入完全喔','錯誤');
+    }
+  });
+
+  $$('#feedback-form').on('submitError', function (e) {
+    console.log(e);
+    myApp.alert('請輸入完全喔','錯誤');
+  });
+
+});
