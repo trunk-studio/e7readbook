fded91fb3635c02d52bb995100d0e87d220085db
fuyaode <dan82625@gmail.com>
2016-03-04 14:16:05 +0800

    fix report add domain & change booklist to page slider

--- app/js/bookList.js
+++ app/js/bookList.js
@@ -15,6 +15,39 @@ $$(document).on('pageInit', '.page[data-page="bookList"]', function (e) {
       myApp.alert(thrownError);
     }
   });
+
+
+  $$(document).on('click', '#bookListUl .item-content', function(){
+    var id = $$(this).attr('data-id');
+    var totalPageNumber = $$(this).attr('data-totalPageNumber');
+    var openUrl = "/ereader?id="+ id +"&name=APPLE&pages="+ totalPageNumber +"&loc=/a/1/a1tw-32sd-23dfs-3f24-sdff-fs3s&uuid=a1tw-32sd-23dfs-3f24-sdff-fs3s";
+    var bookPages = [];
+    $$.ajax({
+      url: openUrl,
+      type:"GET",
+      success: function(result){
+        console.log(JSON.parse(result));
+        var bookDate = JSON.parse(result);
+        if(bookDate.pageTotal == 0){
+          myApp.alert("書本沒有資料喔",'錯誤');
+          return ;
+        }
+        bookDate.pages.forEach(function(book){
+          bookPages.push(book.url);
+        });
+        var myPhotoBrowserStandalone = myApp.photoBrowser({
+          photos : bookPages,
+          swipeToClose: false,
+          expositionHideCaptions: true,
+          lazyLoading: true,
+          lazyLoadingInPrevNext: true,
+          lazyLoadingOnTransitionStart: true
+        });
+        myPhotoBrowserStandalone.open();
+      }
+    });
+  });
+
 })
 
 
